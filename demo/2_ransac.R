e2ddcf135c8aa1bd2596618fb81320b7726d39bf
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-10-27 16:37:49 +0800

    make alignment test

--- demo/2_ransac.R
+++ demo/2_ransac.R
@@ -1,6 +1,8 @@
 imports "geometry2D" from "graphics";
 imports "machineVision" from "signalKit";
 
+require(JSON);
+
 let map = ?"--map" || stop("missing cells table data for make mapping!");
 let subject = ?"--subj" || stop("missing subject cell table data for used as mapping reference!");
 let dir = ?"--out" || relative_work();
@@ -23,17 +25,17 @@ map = as.data.frame(map);
 map[,"class"] = "map"; 
 
 aligned[,"class"] = "aligned(map)";
-aligned = rbind(HE, aligned);
+aligned = rbind(subject, aligned);
 
 let un_aligned = rbind(map, subject);
 
 write.csv(aligned, file = file.path(dir, "RANSAC", "aligned.csv"), row.names = FALSE);
 writeLines(JSON::json_encode(t), con = file.path(dir, "RANSAC", "transform.json"));
 
-bitmap(file = file.path(dir, "RANSAC", "unaligned.png")) {
-    plot(as.numeric(un_aligned$x),as.numeric(un_aligned$y), class = un_aligned$class, fill = "white",point_size= 2, colors = "paper");
+bitmap(file = file.path(dir, "RANSAC", "unaligned.png"), size = [3000,2000]) {
+    plot(as.numeric(un_aligned$x),as.numeric(un_aligned$y), class = un_aligned$class, fill = "white",point_size= 5, colors = "paper");
 }
 
-bitmap(file = file.path(dir, "RANSAC", "aligned.png")) {
-    plot(as.numeric(aligned$x),as.numeric(aligned$y), class = aligned$class, fill = "white",point_size= 2, colors = "paper");
+bitmap(file = file.path(dir, "RANSAC", "aligned.png"), size = [3000,2000]) {
+    plot(as.numeric(aligned$x),as.numeric(aligned$y), class = aligned$class, fill = "white",point_size= 5, colors = "paper");
 }
