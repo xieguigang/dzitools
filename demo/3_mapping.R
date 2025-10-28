a0d6b9dbaac546c3d43bbbfa5ad7f289305ab638
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-10-28 13:52:27 +0800

    export plot image

--- demo/3_mapping.R
+++ demo/3_mapping.R
@@ -16,12 +16,11 @@ let subject = read.cells(relative_work("capture/cells_bin.csv"));
 map = map |> singleCell::geo_transform(t);
 
 let assign = greedy_matches(map, subject);
+let df = as.data.frame(assign);
 
-assign = as.data.frame(assign);
+print(df);
 
-print(assign);
-
-write.csv(assign, file = relative_work("RANSAC/cell_matches.csv"), row.names = FALSE);
+write.csv(df, file = relative_work("RANSAC/cell_matches.csv"), row.names = FALSE);
 
 bitmap(file = relative_work("RANSAC/cell_matches.png"), size = [2700,2100]) {
     plot(assign, slide1 = map, slide2 = subject);
