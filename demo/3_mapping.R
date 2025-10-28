2058347ac9ca9bc64d78685902dbfb858d71802f
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-10-28 15:56:48 +0800

    run alignm,ent and test matches success

--- demo/3_mapping.R
+++ demo/3_mapping.R
@@ -15,13 +15,13 @@ let subject = read.cells(relative_work("capture/cells_bin.csv"));
 
 map = map |> singleCell::geo_transform(t);
 
-let assign = greedy_matches(map, subject);
+let assign = greedy_matches(map, subject, maxDistance = 120);
 let df = as.data.frame(assign);
 
 print(df);
 
 write.csv(df, file = relative_work("RANSAC/cell_matches.csv"), row.names = FALSE);
 
-bitmap(file = relative_work("RANSAC/cell_matches.png"), size = [2700,2100], dpi = 300) {
+bitmap(file = relative_work("RANSAC/cell_matches.png"), size = [3800,2400], dpi = 300, padding = "padding: 2% 2% 10% 10%;") {
     plot(assign, slide1 = map, slide2 = subject);
 }
